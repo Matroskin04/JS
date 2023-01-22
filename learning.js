6ded2f0b151079ca1811cf7e55e81f4a41f85567
EgorMatafonov <zetwyn@mail.ru>
2023-01-22 22:01:34 +0700

    learning

---
+++ learning.js
@@ -0,0 +1,27 @@
+            // 1 Переменные
+
+// const fn = 'Egor'
+// var sn = 'Matafonov' //устаревший вариант
+// let age = 19
+
+// const _$ = 'ds'
+// // const 4kfmeo = 'ds' //err
+
+//             // 2 Мутирование
+            
+// //console.log('Имя человека: ' + fn + ', а возраст: ' + age)
+
+// //const ln = prompt('Введите фамилию: ')
+// //alert(fn + ' ' + ln)
+
+//             // 3 Операторы
+
+// let a = 5
+// let b = 10
+// let c = 1
+
+// console.log(++b, --a)
+// c *= a // то же,что и "c = c + a"
+// console.log(c)
+
+//             // 4  
