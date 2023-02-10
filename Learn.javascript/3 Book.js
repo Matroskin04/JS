35ea4dddf1c45389539ded65ca8761bc6b937a1f
EgorMatafonov <zetwyn@mail.ru>
2023-02-10 10:11:15 +0700

    10.02.23

---
+++ Learn.javascript/3 Book.js
@@ -0,0 +1,40 @@
+let calculator = {
+    read() {
+        calculator['a'] = prompt('Введите 1 числo', '4');
+        calculator['b'] = prompt('Введите 2 числo', '4');
+    },
+    sum() {
+        return +calculator['a'] + +calculator['b'];
+    },
+    mul() {
+        return calculator['a'] * calculator['b'];;
+    },
+}
+
+
+/* Variant 2 drom the book
+let calculator = {
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  read() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  }
+};
+*/
+
+
+
+/*
+Создайте объект calculator (калькулятор) с тремя методами:
+
+read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
+sum() (суммировать) возвращает сумму сохранённых значений.
+mul() (умножить) перемножает сохранённые значения и возвращает результат.
+*/
