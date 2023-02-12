7403f8ad8ae1dad63610a88e9eabee567198d7bb
EgorMatafonov <zetwyn@mail.ru>
2023-02-12 14:29:35 +0700

    12.02.23

--- Learn.javascript/Task 1.js
+++ Learn.javascript/Task 1.js
@@ -1,1 +1,14 @@
 'use strict'
+function pairZeros(arr) {
+    for ( let i = 0; i < arr.length; i++ ) {
+      if ( arr[i] === 0 ) {
+        for ( let j = i + 1; j < arr.length; j++ ) {
+          if ( arr[i] === arr[j] ) {
+            arr.splice(j, 1)
+          }
+        }
+      }
+    }
+    console.log(arr)
+  }
+  pairZeros([1,0,1,0,2,0,0,3,0])
