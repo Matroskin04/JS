fb98b1138e3dbbdcd8281ca0bfdb4d234c3b5a21
EgorMatafonov <zetwyn@mail.ru>
2023-02-18 23:13:51 +0700

    18.02.23

---
+++ .vscode/Interesting Moments/Function within a function.js
@@ -0,0 +1,58 @@
+//1
+function a() {
+    alert('A');
+  }
+  //alerts 'A', returns undefined
+  
+  function b() {
+    alert('B');
+    return a;
+  }
+  //alerts 'B', returns function a
+  
+  function c() {
+    alert('C');
+    return a();
+  }
+  //alerts 'C', alerts 'A', returns undefined
+  
+  alert("Function 'a' returns " + a());
+  alert("Function 'b' returns " + b());
+  alert("Function 'c' returns " + c());
+
+
+
+//2
+function a() {
+    alert("A!");
+  
+    function b() {
+      alert("B!");
+    }
+  
+    return b();
+  }
+  
+  var s = a();
+  alert("break");
+  s();
+  // Output: A! - B! - break
+
+
+
+  //3
+  function a() {
+  
+    alert('A!');
+
+    function b(){
+        alert('B!'); 
+    }
+
+    return b;
+}
+
+var s = a();
+alert('break');
+s();
+// Output: A! - break - B!
