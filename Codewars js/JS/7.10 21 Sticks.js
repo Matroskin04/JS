879b3d8279a8d570f7c08dfbb298f19171dc3f12
EgorMatafonov <zetwyn@mail.ru>
2023-02-21 00:16:03 +0700

    21.02.23

---
+++ Codewars js/JS/7.10 21 Sticks.js
@@ -0,0 +1,39 @@
+function makeMove(sticks) {
+    if ( sticks % 4 === 1) {
+      return 1;
+    };
+    if ( sticks % 4 === 2) {
+      return 2;
+    };
+      if ( sticks % 4 === 3) {
+      return 3;
+    };
+  }
+
+
+//Another solution from Codewars
+function makeMove(sticks) {
+    return sticks % 4
+  }
+
+
+/*
+The game
+In this game, there are 21 sticks lying in a pile. Players take turns taking 1, 2, or 3 sticks. The last person to take a stick wins. For example:
+
+21 sticks in the pile
+
+Bob takes 2  -->  19 sticks left
+Jim takes 3  -->  16 sticks
+Bob takes 3  -->  13 sticks
+Jim takes 1  -->  12 sticks
+Bob takes 2  -->  10 sticks
+Jim takes 2  -->   8 sticks
+Bob takes 3  -->   5 sticks
+Jim takes 3  -->   2 sticks
+Bob takes 2  -->  Bob wins!
+Your task
+Create a robot that will always win the game. Your robot will always go first. The function should take an integer and returns 1, 2, or 3.
+
+Note: The input will always be valid (a positive integer)
+*/
