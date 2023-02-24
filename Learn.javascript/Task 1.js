f0ab5a6356fa83447af5dcda6e725572958e0c03
EgorMatafonov <zetwyn@mail.ru>
2023-02-24 10:29:18 +0700

    24.02.23

--- Learn.javascript/Task 1.js
+++ Learn.javascript/Task 1.js
@@ -1,14 +1,13 @@
 'use strict'
-function readNumber() {
-  let a = prompt('Введите число', '');
-  
-  for (let i = 0; ; i++) {
-  if (a === 'null' || a === '') {
-      return( 'Отмена' );
-  } else if (typeof a === 'number') {
-      return a;
-  } else a = prompt('Введите число', '');
+function sumOfIntegersInString(s){
+  let sum = 0;
+  for ( let i = 0; i < s.length; i++ ) {
+    if (!isNaN(s[i])) {
+      let num = parseInt(s.slice(i));
+      i += num.toString().length - 1;
+      sum += num;
   }
+} console.log(sum)
 }
 
-readNumber()
+sumOfIntegersInString('123asd123s2s0x1')
