7b9be600f44d66748c4e82a7a51abab5d7c7ccdd
Matroskin04 <zetwyn@mail.ru>
2023-04-28 19:11:57 +0700

    tasks

--- Learn.javascript/15.js
+++ Learn.javascript/15.js
@@ -1,12 +1,11 @@
 'use strict'
 
-function filterRange(arr, a, b) {
-    return arr.filter(item => ( item >= a && item <= b ) )
-}
+// function filterRange(arr, a, b) {
+//     return arr.filter(item => ( item >= a && item <= b ) )
+// }
 
-let a = {name: 'john'}
-let map = new WeakMap()
-map.set(a, 'hi')
-a = 1
-console.log(map)
-
+// let a = {name: 'john'}
+// let map = new WeakMap()
+// map.set(a, 'hi')
+// a = 1
+// console.log(map)
