1e3c45a51a76693fe412f43fa9a60653639b4ee5
Matroskin04 <zetwyn@mail.ru>
2024-01-22 11:08:30 +0700

    commit1

--- Codewars js/JS/4 kyu/4.3 Counting Change Combinations.js
+++ Codewars js/JS/4 kyu/4.3 Counting Change Combinations.js
@@ -2,6 +2,7 @@ function countChange(amount, denominations) {
     let ways = new Array(amount + 1).fill(0);
     ways[0] = 1; 
 
+    
     for (let coin of denominations) {
         for (let i = coin; i <= amount; i++) {
             ways[i] += ways[i - coin];
